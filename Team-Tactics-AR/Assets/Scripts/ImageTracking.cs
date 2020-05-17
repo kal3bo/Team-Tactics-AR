d630e3c6a6f641ce719e2ab9121ef1f8a5a3cbc8
Diego Kaleb Valenzuela Carrillo <a01018992@itesm.mx>
2020-05-16 19:53:13 -0700

    Image Tracking
    - Placing a Soccer Field when recognizing one image.
    - APK updated.

---
+++ Team-Tactics-AR/Assets/Scripts/ImageTracking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageTracking : MonoBehaviour
+{
+    // Private variables:
+    private ARTrackedImageManager arTrackedImageManager;
+
+    //Private Methods:
+
+    private void Awake()
+    {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+
+    }
+}
